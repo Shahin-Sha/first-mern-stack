89bd827d53862890c3b9d1215bab35183a015fca
Shahin-Sha <shahinshakilinadan@gmail.com>
2023-05-26 16:45:52 +0530

    final

--- backend/app.js
+++ backend/app.js
@@ -4,29 +4,35 @@ const cors = require('cors'); //!for cross orgin policy
 const PORT = 8745
 app.use(cors()); //! "app.use(cors())" means use this cors package when server starts up
 
+
 app.use(express.json()); //! to parse json data
 app.use(express.urlencoded({extended:true})); //! to parse url encoded data
 app.use(express.static('public')); //! to serve static files
 
 
+
 // DB 
 require('./dB') //!mongoose DB connection folder is called here hen server starts up
-const PRODUCT = require('./product')
+const PRODUCT = require('./models/product')
 
 
-app.post('/product',async (req,res)=>{
+// To add product details coming from front end to backend database 
+app.post('/products',async (req,res)=>{
     try {
         console.log(req.body);
         let item = req.body;
+
 
         const saveItem = PRODUCT(item);
         await saveItem.save();
         res.send("Success!");
 
+
     } catch (error) {
         res.send(error.message)
     }
 })
+
 
 
 app.listen(PORT,()=>{
